0ec85948f5e60c3474fd7099b496b27be848602e
Jake <jakehcasto@aol.com>
2016-07-29 16:28:56 -0400

    Add files via upload

---
+++ src/EconomyPlus/EconomyPlayer.php
@@ -0,0 +1,63 @@
+<?php
+namespace EconomyPlus;
+
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\Plugin;
+
+use EconomyPlus\Main;
+
+use pocketmine\utils\Config;
+
+/* Copyright (C) ImagicalGamer - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+ */
+
+class EconomyPlayer extends PluginBase{
+
+  public function __construct(Main $plugin, Player $player, $hasFile = null){
+    $this->plugin = $plugin;
+    $this->player = $player;
+    $this->hasFile = $hasFile;
+    $this->cfg = new Config($this->plugin->getDataFolder() . "/players.yml", Config::YAML);
+  }
+
+  public function getMoney(){
+    return $this->cfg->get(strtolower($this->player->getName()));
+  }
+
+  public function subtractMoney(int $ammount){
+    $money = $this->getMoney();
+    if($money > $ammount){
+      $this->setMoney($money - $ammount);
+    }
+    else{
+      return false;
+    }
+  }
+
+  public function setMoney(int $ammount){
+    $this->cfg->set(strtolower($this->player->getName()), round($ammount));
+    $this->cfg->save();
+    return true;
+  }
+
+  public function addMoney(int $ammount){
+    $this->setMoney($ammount + $this->getMoney());
+    return true;
+  }
+
+  public function newPlayer(){
+    $this->cfg->set(strtolower($this->player->getName()), $this->plugin->cfg->get("Default-Money"));
+    $this->cfg->save();
+    return true;
+  }
+
+  public function sendMessage(String $message){
+    $this->player->sendMessage($message);
+    return true;
+  }
+}
---
+++ src/EconomyPlus/EventListener.php
@@ -0,0 +1,35 @@
+<?php
+namespace EconomyPlus;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use EconomyPlus\Main;
+
+use pocketmine\event\player\PlayerJoinEvent;
+
+/* Copyright (C) ImagicalGamer - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+ */
+
+class EventListener extends PluginBase implements Listener{
+
+  protected $plugin;
+
+  public function __construct(Main $plugin)
+  {
+    $this->plugin = $plugin;
+  }
+
+  public function onJoin(PlayerJoinEvent $event){
+    $player = new EconomyPlayer($this->plugin, $event->getPlayer());
+    if(!$this->plugin->cfg->get(strtolower($event->getPlayer()->getName())) instanceof int){
+      $player->newPlayer();
+    }
+  }
+}
