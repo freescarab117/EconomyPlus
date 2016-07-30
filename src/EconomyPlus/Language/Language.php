491fc7380c2aff6886ac5f259e64898bd38a67c4
Jake <jakehcasto@aol.com>
2016-07-30 11:40:27 -0400

    Add files via upload

--- src/EconomyPlus/Language/Language.php
+++ src/EconomyPlus/Language/Language.php
@@ -18,13 +18,23 @@ use pocketmine\utils\Config;
 
 class Language extends PluginBase{
 
-  public function __construct(Main $plugin, String $message, String $lang = "eng"){
+  public function __construct(Main $plugin, String $message, String $lang = "eng", String $type = null){
     $this->plugin = $plugin;
     $this->message = $message;
     $this->lang = $lang;
+    $this->type = $type;
+    $this->langFile = new Config($this->getDataFolder() . "/Language/" . $this->lang . ".yml", Config::YAML);
   }
 
   public function translate(){
     return $this->message;
+  }
+
+  public function getMessageType(){
+  	return $this->type;
+  }
+
+  public function getLang(){
+  	return $this->lang;
   }
 }
