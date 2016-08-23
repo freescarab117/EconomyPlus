fe0f5a4d8e97277923a7c3cd48a092c667b660c8
Jake <jakehcasto@aol.com>
2016-08-23 19:53:56 -0400

    Update TopMoneyTask.php

--- src/EconomyPlus/Tasks/TopMoneyTask.php
+++ src/EconomyPlus/Tasks/TopMoneyTask.php
@@ -4,63 +4,56 @@ namespace EconomyPlus\Tasks;
 use pocketmine\Player;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 use pocketmine\plugin\Plugin;
-
-use EconomyPlus\Main;
 
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
-use pocketmine\item\Item;
 
 /* Copyright (C) ImagicalGamer - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+ * Written by Jake C <imagicalgamer@outlook.com>, August 2016
  */
 
-class TopMoneyTask extends PluginBase{
+class TopMoneyTask extends AsyncTask{
 
-  public function __construct(Main $plugin, String $player, Array $allMoney){
-  	$this->plugin = $plugin;
+  public function __construct(String $player, Array $allMoney){
     $this->player = $player;
     $this->allMoney = $allMoney;
   }
 
-  public function getPlayer(){
-  	if($this->player == "CONSOLE"){
-  		return;
-  	}
-  	return $this->plugin->getServer()->getPlayer($this->player);
-  }
-
   public function onRun(){
-  	$money = $this->allMoney;
-  	arsort($money);
-  	$ret = array();
-  	$n = 0;
-  	$this->getPlayer()->sendMessage(C::GREEN . "EconomyPlus TopMoney!");
-    $this->getPlayer()->sendMessage(C::YELLOW . "---------------------");
+  }
+  
+  public function onCompletion(Server $server){
+    $money = $server->getPluginManager()->getPlugin("EconomyPlus")->allMoney();
+    arsort($money);
+    $ret = array();
+    $n = 0;
+    $server->getPlayer($this->player)->sendMessage(C::GREEN . "EconomyPlus TopMoney!");
+    $server->getPlayer($this->player)->sendMessage(C::YELLOW . "---------------------");
     foreach($money as $p => $m){
       $n++;
       $ret[$n] = [$p, $m];
       $message = json_encode($ret[$n]);
       $message = str_replace(["[", "]", '"'], "", $message);
       $message = str_replace(",", "§e: $", $message);
-      if($p == strtolower($this->getPlayer()->getName())) {
-      $this->getPlayer()->sendMessage(C::GREEN . C::BOLD . "* Your Rank: " . C::RESET . C::YELLOW . $n . "\n  ");
+      if($p == strtolower($server->getPlayer($this->player)->getName())) {
+      $server->getPlayer($this->player)->sendMessage(C::GREEN . C::BOLD . "* Your Rank: " . C::RESET . C::YELLOW . $n . "\n  ");
       }
     }
     $n = 0;
-  	foreach($money as $p => $m){
-  		$n++;
-  		$ret[$n] = [$p, $m];
-  		$message = json_encode($ret[$n]);
-  		$message = str_replace(["[", "]", '"'], "", $message);
-  		$message = str_replace(",", "§e: $", $message);
-  		if($n > 10){
-  			return $n = $n - 10;
-  		}
-  		$this->getPlayer()->sendMessage(C::GREEN . "* " . C::YELLOW . $n . ". " . C::GREEN .  $message);
-  	}
+    foreach($money as $p => $m){
+      $n++;
+      $ret[$n] = [$p, $m];
+      $message = json_encode($ret[$n]);
+      $message = str_replace(["[", "]", '"'], "", $message);
+      $message = str_replace(",", "§e: $", $message);
+      if($n > 10){
+        return $n = $n - 10;
+      }
+      $server->getPlayer($this->player)->sendMessage(C::GREEN . "* " . C::YELLOW . $n . ". " . C::GREEN .  $message);
+    }
   }
 }
