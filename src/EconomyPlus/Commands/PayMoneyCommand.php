98c32bef2cfa1bad97625fd33496303e92675cc4
Jake <jakehcasto@aol.com>
2016-09-12 20:03:42 -0400

    Possible Fix #5

--- src/EconomyPlus/Commands/PayMoneyCommand.php
+++ src/EconomyPlus/Commands/PayMoneyCommand.php
@@ -13,7 +13,7 @@ use EconomyPlus\EconomyPlayer;
 /* Copyright (C) ImagicalGamer - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Jake C <imagicalgamer@outlook.com>, July 2016
+ * Written by Jake C <imagicalgamer@outlook.com>, September 2016
  */
 
 class PayMoneyCommand extends BaseCommand{
@@ -41,6 +41,11 @@ class PayMoneyCommand extends BaseCommand{
         }
         if(!is_numeric($args[1])){
             $sender->sendMessage(C::RED . $this->plugin->translate("INVALID-AMOUNT"));
+            return;
+        }
+        $player2 = new EconomyPlayer($this->plugin, $sender->getName());
+        if(!$player2->getMoney() >= $args[0]){
+            $sender->sendMessage(C::RED . $this->plugin->translate("INVALID-BALANCE"));
             return;
         }
         $player = new EconomyPlayer($this->plugin, $args[0]);
