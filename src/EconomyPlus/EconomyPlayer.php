8583f6eb4a64235343d9b7f1f56e0899bc07e956
Jake <jakehcasto@aol.com>
2016-09-15 17:05:36 -0400

    Update EconomyPlayer.php

--- src/EconomyPlus/EconomyPlayer.php
+++ src/EconomyPlus/EconomyPlayer.php
@@ -35,7 +35,7 @@ class EconomyPlayer extends PluginBase{
     $this->plugin = $plugin;
     $this->player = $player;
     $this->cfg = new Config($this->plugin->getDataFolder() . "/players.json", Config::JSON);
-    if(!$this->cfg->exists(strtolower($player)))
+    if($this->cfg->exists(strtolower($player)) == null)
     {
       $this->cfg->set(strtolower($this->player), $this->plugin->cfg->get("Default-Money"));
       $this->cfg->save();
